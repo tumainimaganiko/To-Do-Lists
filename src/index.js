0c44a67a667ffbf0e0e6926f2d861d954030cf4d
tumainimaganiko <tumainimaganiko@gmail.com>
2023-05-02 12:43:27 +0300

    Create Array of Objects

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 import _ from 'lodash';
 import './style.css';
+
+const myTodo = [
+    {description:"Going to the GYM",completed:true,index:1},
+    {description:"Cleaning my Room", completed:false, index:2 },
+    {description:"Washing my Car", completed:true, index:3},
+    {description:"Coding for 4 hours", completed:false, index:4 }
+]
