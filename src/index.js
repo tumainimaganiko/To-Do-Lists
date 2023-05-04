4d1a31ca83d60f68170f15fc2a76eb757567124f
tumainimaganiko <tumainimaganiko@gmail.com>
2023-05-04 16:15:17 +0300

    Remove Hardcoded code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import './style.css';
 
-const myTodo = [
-  { description: 'Going to the GYM', completed: true, index: 1 },
-  { description: 'Cleaning my Room', completed: false, index: 2 },
-  { description: 'Washing my Car', completed: true, index: 3 },
-  { description: 'Coding for 4 hours', completed: false, index: 4 },
-];
+let myTodo = JSON.parse(localStorage.getItem("todo")) || [];
+
 const list = document.getElementById('list');
 // Sorting the array first
 myTodo.sort((a, b) => a.index - b.index);
