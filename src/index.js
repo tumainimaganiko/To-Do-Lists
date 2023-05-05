9098e41d40aa0e6fecd0a773898d59742f4d3b33
tumainimaganiko <tumainimaganiko@gmail.com>
2023-05-05 09:19:12 +0300

    Update Completed key

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import './style.css';
-// import {
-//   add, remove, retrieve, save,
-// } from './module/utils.js';
-import utils from './module/utils.js';
-// import   add, remove, save, retrieve, from './module/utils.js'
+import utils from './modules/utils.js';
+import { completed, unCompleted } from './modules/status';
 
 const localData = utils.retrieve();
 if (!localData) localStorage.setItem('todo', '[]');
@@ -17,7 +14,7 @@ const display = () => {
     // Creating list of to-do
     const li = document.createElement('li');
     li.innerHTML = `
-    <input type="checkbox">
+    <input class="checkbox" type="checkbox">
     <input class="text" type="text" value="${value.description}"/> 
     <i class="fa-solid fa-ellipsis-vertical"></i>
     `;
@@ -39,6 +36,19 @@ const display = () => {
       utils.save(test);
     });
   });
+
+  const checkbox = document.querySelectorAll('.checkbox');
+checkbox.forEach((btn,index) => {
+  btn.addEventListener('change', () => {
+      const test = utils.retrieve();
+      if(btn.checked){
+      test[index].completed = completed(test);
+    }else {
+      test[index].completed = unCompleted(test);
+    }
+    utils.save(test)
+  })
+})
 };
 
 const renderList = () => {
@@ -47,7 +57,7 @@ const renderList = () => {
     e.preventDefault();
     const storeData = utils.retrieve();
     const input = form.text.value;
-    const completed = false;
+    let completed = false;
     const id = storeData.length;
 
     utils.add(input, completed, id);
